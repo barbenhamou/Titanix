fca70ed566b0e6012f273976328e79b462684c28
barbenhamou <barbenhamou2006@gmail.com>
2023-12-04 22:32:44 +0200

    cleaning stuff

--- include/lib/interrupts.h
+++ include/lib/interrupts.h
@@ -19,6 +19,44 @@ typedef struct idtr_t {
     uint64_t base;
 } __attribute__((packed)) idtr_t;
 
+typedef struct isr_frame_t {
+    struct {
+        uint64_t cr4;
+        uint64_t cr3;
+        uint64_t cr2;
+        uint64_t cr0;
+    } __attribute__((packed)) control_registers;
+
+    struct {
+        uint64_t r15;
+        uint64_t r14;
+        uint64_t r13;
+        uint64_t r12;
+        uint64_t r11;
+        uint64_t r10;
+        uint64_t r9;
+        uint64_t r8;
+        uint64_t rdi;
+        uint64_t rsi;
+        uint64_t rdx;
+        uint64_t rcx;
+        uint64_t rbx;
+        uint64_t rax;
+    } __attribute__((packed)) general_purpose_registers;
+
+    struct {
+        uint64_t rbp;
+        uint64_t vector;
+        uint64_t error_code;
+        uint64_t rip;
+        uint64_t segment_selector;
+        uint64_t rflags;
+        uint64_t rsp;
+        uint64_t dss;
+    } __attribute__((packed)) basic_frame;
+    
+} __attribute__((packed)) isr_frame_t;
+
 #define IDT_MAX_DISCRIPTORS 256
 #define IDT_MAX_CPU_EXCEPTIONS 32
 
@@ -33,11 +71,5 @@ uint8_t idt_allocate_vector();
 void idt_free_vector(uint8_t vector);
 
 void print_idt();
-
-// typedef struct registers_t {
-//     uint64_t ds;
-//     uint64_t rax, rbx, rcx, rdx, rsi, rdi, rbp, r8, r9, r10, r11, r12, r13, r14, r15;
-
-// }
 
 #endif
---
+++ src/interrupts/isr.s
@@ -0,0 +1,63 @@
+%ifndef ISR__S 
+%define ISR__S 
+
+section .text
+bits 64
+
+global isr_stub_table
+extern exception_handler
+
+%macro isr_error 1
+    isr%+%1:
+        call exception_handler
+        iretq
+%endmacro
+
+%macro isr_no_error 1
+    isr%+%1:
+        call exception_handler
+        iretq
+%endmacro
+
+isr_no_error 0
+isr_no_error 1
+isr_no_error 2
+isr_no_error 3
+isr_no_error 4
+isr_no_error 5
+isr_no_error 6
+isr_no_error 7
+isr_error 8
+isr_no_error 9
+isr_error 10
+isr_error 11
+isr_error 12
+isr_error 13
+isr_error 14
+isr_no_error 15
+isr_no_error 16
+isr_error 17
+isr_no_error 18
+isr_no_error 19
+isr_no_error 20
+isr_no_error 21
+isr_no_error 22
+isr_no_error 23
+isr_no_error 24
+isr_no_error 25
+isr_no_error 26
+isr_no_error 27
+isr_no_error 28
+isr_no_error 29
+isr_error 30
+isr_no_error 31
+
+isr_stub_table:
+    %assign i 0
+    %rep 32
+        dq isr%+i
+        %assign i i+1
+    %endrep
+
+
+%endif
